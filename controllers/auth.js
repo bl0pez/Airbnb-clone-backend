9c0ed92d0ee7004ab8d3c68b7fb6ceecfb49c2d5
bl0pez <blopez4434@gmail.com>
2023-03-19 01:03:42 -0300

    agregando manejo de errores

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,13 +4,20 @@ const register = async(req, res) => {
 
     const { name, email, password } = req.body;
 
-    const user = await User.create({
-        name,
-        email,
-        password
-    });
-
-    res.status(201).json(user)
+    try {
+        const user = await User.create({
+            name,
+            email,
+            password
+        });
+    
+        res.status(201).json(user)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msj: "Internal server error"
+        });
+    }
 
 }
 
